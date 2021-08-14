79187fc26eccf868cbb2a078eec90e4a131ac8ec
dumi123wabot <88726327+dumi123wabot@users.noreply.github.com>
2021-08-15 03:03:54 +0530

    Create dpack.js

---
+++ dpack.js
@@ -0,0 +1,121 @@
+🔱 Command: .skalt
+🧿Description: *It Converts Text to Logo
+
+🔱 Command: .ug
+🧿Description: *It Converts Text to Logo
+
+🔱 Command: .wicker
+🧿Description: *It Converts Text to Logo
+
+🔱 Command: *.hwf
+*🧿Description: *It Converts Text to  logo
+
+🔱 Command: .neon
+🧿Description: It Converts Text to logo
+
+🔱 Command: .nt
+🧿Description: It Converts Text to Logo
+
+🔱 Command: .gc
+🧿Description: It Converts Text to Logo
+
+🔱 Command: .dg
+🧿Description: It Converts Text to Logo
+
+🔱 Command: .chocolotcake
+🧿Description: It Converts Text to Logo
+
+🔱 Command: .rock
+🧿Description: It Converts Text to Logo
+
+🔱 Command: .lava
+🧿Description: It Converts Text to Logo
+
+🔱 Command: .yg
+🧿Description: It Converts Text to logo
+
+🔱 Command: .robot
+🧿Description: It Converts Text to logo
+
+🔱 Command: .gs
+🧿Description: It Converts Text to logo
+
+🔱 Command: .iv
+🧿Description: It Converts Text to Logo
+
+🔱 Command: .cloud
+🧿Description: It Converts Text to logo
+
+🔱 Command: .marvals
+🧿Description: It Converts Text to Marval Studio logo
+
+🔱 Command: .wolfblack
+🧿Description: It Converts Text to black wolf logo
+
+🔱 Command: .steel
+🧿Description: It Converts Text to logo
+
+🔱 Command: .cfnr
+🧿Description: It Converts Text to Logo
+
+🔱 Command: .sandwriting
+🧿Description: *It  Text to Logo *
+
+🔱 Command: .sandsb
+🧿Description: It Converts Text to logo
+
+🔱 Command: .csgo
+🧿Description: It Converts Text to logo
+
+🔱 Command: .lp
+🧿Description: It Converts Text to Logo
+
+🔱 Command: .m7l
+🧿Description: It Converts Text to Logo
+
+🔱 Command: .aov
+🧿Description: It Converts Text to logo
+
+🔱 Command: .pokemon
+🧿Description: It Converts Text to logo
+
+🔱 Command: .bevel
+🧿Description: It Converts Text to Logo
+
+🔱 Command: .smoketypography
+🧿Description: It Converts Text to Logo
+
+🔱 Command: .rainbowbg
+🧿Description: It Converts Text to logo
+
+🔱 Command: .kueultah
+🧿Description: It Converts Text to logo
+
+🔱 Command: .fur
+🧿Description: It Converts Text to Logo
+
+🔱 Command: .crispchrome
+🧿Description: It Converts Text to logo
+
+🔱 Command: .stars
+🧿Description: It Converts Text to logo
+
+🔱 Command: .underwebmatrix
+🧿Description: It Converts Text to logo
+
+🔱 Command: .underwebmatrix
+🧿Description: It Converts Text to logo
+
+🔱 Command: .lolcover2
+🧿Description: It Converts Text to logo
+
+🔱 Command: .coveroverwatch
+🧿Description: It Converts Text to logo
+
+🔱 Command: .warfacecover
+🧿Description: It Converts Text to logo
+
+KING D-C LOGO PACK
+
+
+*සමහර ලොගෝ වැඩ නෑ.ඒ වරද අපේ බොට්ගේ නොවේ ලොගෝ දෙන සයිට් වල limit එක බැවින් අපිට එයට කරන්න දෙයක් නැත.
